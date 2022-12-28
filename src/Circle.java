fb528e4aff20d1c2608b599c5bcb7247835bed81
kiran1090 <63589265+kiran1090@users.noreply.github.com>
2022-12-28 10:32:44 +0530

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Circle {
+package PACKAGE_NAME;
+public class Circle {
+  // Instance variable
+private double radius;
+
+/** Constructs a Circle instance with the given radius and default color */
+public Circle(double radius) {
+   this.radius = radius;   // "this.radius" refers to the instance variable
+                           // "radius" refers to the method's parameter
+   color = "red";
+}
+
+/** Sets the radius to the given value */
+public void setRadius(double radius) {
+   this.radius = radius;   // "this.radius" refers to the instance variable
+                           // "radius" refers to the method's argument
+}
 }
